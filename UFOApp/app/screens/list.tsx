b93ebb698ec4998d1630323b238990e05be0c9fe
Kagermanov Vislan (s144510) <vislan.kagermanov@student.ap.be>
2025-02-15 18:57:00 +0100

    add fetching sightings on list screen

--- UFOApp/app/screens/list.tsx
+++ UFOApp/app/screens/list.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, ActivityIndicator, Image } from "react-native";
+
+interface Sighting {
+  id: number;
+  picture: any;
+  witnessName: string;
+  dateTime: string;
+  city: string;
+  date: string;
+  description?: string;
+}
 
 export default function ListScreen() {
+  const [sightings, setSightings] = useState<Sighting[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://sampleapis.assimilate.be/ufo/sightings')
+      .then(response => response.json())
+      .then(data => {
+        setSightings(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text> TO DO hier komt de lijst </Text>
+    <View>
+      <FlatList
+        data={sightings}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={{ uri: item.picture }} style={{ width: 100, height: 100 }} />
+            <Text>{item.witnessName}</Text>
+            <Text>{new Date(item.dateTime).toLocaleString()}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+
+
 
 
